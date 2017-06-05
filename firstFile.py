acdd941226e65c6773b2f9a6ea6d186dfe563123
Mangesh Kumar <mangesh.kumar300@gmail.com>
2017-06-05 10:39:35 +1000

    Initial commit

---
+++ firstFile.py
@@ -0,0 +1,14 @@
+print ("Hello Mangesh...")
+
+surname = "Rajpure.."
+
+print (surname)
+num=int(input("Please enter a number"))
+if num >= 0:
+    if  num == 0:
+        print("zero")
+    else:
+        print("Positive number")
+        print("great...")
+else:
+    print("Negative")
